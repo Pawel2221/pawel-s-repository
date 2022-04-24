49abf7604a2881e3a0b819ece07e8aaea8d3eae4
Pawel2221 <60663217+Pawel2221@users.noreply.github.com>
2022-04-24 22:20:33 +0200

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,1 +1,18 @@
-cnxzjcnkzjxc
+//Connecting CRM data with Google Analytics 4 (in BigQuery)
+
+SELECT
+  utm_source,
+  utm_medium,
+  Application,
+  Accepted_application,
+  value,
+FROM
+  `qwiklabs-gcp-02-2ac57b8d790f.bqtest.CRM_data`
+LEFT JOIN
+  `qwiklabs-gcp-02-2ac57b8d790f.bqtest.Analytics_data`
+ON
+  crm_form_id = form_id
+  AND crm_user_id = user_id
+WHERE utm_source IS NULL THEN COUNT(utm_source AS not_set)
+LIMIT
+  10
